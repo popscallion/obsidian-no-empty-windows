7acee8fee30a844c5dcec0486620845b54bc1cac
L Fahn-Lai <l.fahnlai@gmail.com>
2023-02-26 20:22:17 -0500

    add settings, fix bug with multiple tab groups+popouts

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # no-empty-windows
 
-This plugin provides an alternative to the "Close current tab" command in Obsidian ("workspace:close"), and makes Obsidian windows behave more like MacOS browser windows.
+This plugin provides an alternative to the "Close current tab" command in Obsidian ("workspace:close") for users who prefer more browser-like behavior where closing the last open tab closes the window but leaves the app running in the background for quicker relaunch.
 
-"Close current tab or empty window" closes the currently focused tab unless it is pinned, in which case it does nothing. If there is only one tab left in the currently focused window, the command closes the window but leaves Obsidian running in the background, which allows it to relaunch faster and helps Obsidian URIs work better.
+"Close current tab or empty window" closes the currently focused tab unless it is pinned, in which case it either unpins the tab or closes it directly (toggleable via plugin settings). If tabs are open in multiple tab groups/popouts, the plugin will cycle through all open tabs until only one remains in the main window.
 
 ## Usage
+
 To use this command, either invoke it from the Command Palette or map it to the Close current tab hotkey (cmd+W on MacOS by default).
 
 ## Installing
--- main.ts
+++ main.ts
@@ -1,19 +1,111 @@
-import { Plugin, WorkspaceLeaf } from 'obsidian';
+import {
+  App,
+  Plugin,
+  PluginSettingTab,
+  Setting,
+  WorkspaceLeaf,
+} from "obsidian";
+
+interface MyPluginSettings {
+  unpin: boolean;
+  allowEmpty: boolean;
+}
+
+const DEFAULT_SETTINGS: MyPluginSettings = {
+  unpin: true,
+  allowEmpty: true,
+};
+
+class SettingTab extends PluginSettingTab {
+  plugin: NoEmptyWindows;
+  constructor(app: App, plugin: NoEmptyWindows) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
+  display(): void {
+    const { containerEl } = this;
+    containerEl.empty();
+    new Setting(containerEl)
+      .setName("Unpin tabs")
+      .setDesc("Enable to unpin pinned tabs instead of closing them directly.")
+      .addToggle((toggle) =>
+        toggle.setValue(this.plugin.settings.unpin).onChange(async (value) => {
+          this.plugin.settings.unpin = value;
+          await this.plugin.saveSettings();
+        })
+      );
+    new Setting(containerEl)
+      .setName("Allow empty tabs")
+      .setDesc(
+        "Disable to close the window immediately when the last tab is closed, instead of spawning an empty tab."
+      )
+      .addToggle((toggle) =>
+        toggle
+          .setValue(this.plugin.settings.allowEmpty)
+          .onChange(async (value) => {
+            this.plugin.settings.allowEmpty = value;
+            await this.plugin.saveSettings();
+          })
+      );
+  }
+}
 export default class NoEmptyWindows extends Plugin {
+  settings: MyPluginSettings;
   async onload() {
+    await this.loadSettings();
+    this.addSettingTab(new SettingTab(this.app, this));
     this.addCommand({
       id: "close-tab-or-empty-window",
       name: "Close current tab or empty window",
       callback: () => {
-        const workspace = this.app.workspace
-        const tabGroup = workspace.activeTabGroup
-        const tabs = tabGroup.children
-        if (tabs.length == 1) workspace.getFocusedContainer().win.close()
-        const active = tabs[tabGroup.currentTab]
-        if (!active.pinned) active.detach()
+        const workspace = this.app.workspace;
+        const leaves: WorkspaceLeaf[] = [];
+        workspace.iterateAllLeaves((leaf) => {
+          leaf.view.navigation && leaves.push(leaf);
+        });
+        const activeLeaf = workspace.getMostRecentLeaf();
+        let activeGroup = activeLeaf!.parent;
+        switch (true) {
+          case this.settings.unpin && activeLeaf?.getViewState().pinned:
+            activeLeaf!.setPinned(false);
+            break;
+          case leaves.length === 1 &&
+            activeLeaf!.view.getViewType() === "empty":
+            activeLeaf!.getContainer().win.close();
+            break;
+          case leaves.length === 1 &&
+            (!this.settings.allowEmpty ||
+              (this.settings.allowEmpty &&
+                activeLeaf!.view.getViewType() === "empty")):
+            activeLeaf!.getContainer().win.close();
+            break;
+          case leaves.length > 1 && activeGroup.children.length === 1:
+            const relatives = activeGroup.parent.children;
+            const activeGroups =
+              relatives.length > 1
+                ? relatives
+                    .filter((child: WorkspaceLeaf) => child != activeGroup)
+                    .sort(
+                      (a: any, b: any) => a.children.length - b.children.length
+                    )
+                : [...new Set(leaves.map((leaf: any) => leaf.parent))]
+                    .filter((group) => group != activeGroup)
+                    .sort((a, b) => a.children.length - b.children.length);
+            activeGroup = activeGroups[0];
+            const activeLeaves = activeGroup.children;
+            const nextActiveLeaf = activeLeaves[activeGroup.currentTab];
+            workspace.setActiveLeaf(nextActiveLeaf, { focus: true });
+          default:
+            activeLeaf!.detach();
+        }
       },
     });
   }
-  onunload() {
+  onunload() {}
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
+  async saveSettings() {
+    await this.saveData(this.settings);
   }
 }
